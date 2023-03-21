f6f6380d0ecf4307c88fff2f8919aa3bbe8fb330
AbdyraevNyrgazy <abdyraevnyrgazy@gmail.com>
2023-03-21 13:49:57 +0600

    dz2

---
+++ dz2/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from "react";
+
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      time: new Date()
+    }
+  }
+  upDateTime = () =>{
+    this.setState({
+      time: new Date()
+    })
+  }
+    intervalid = setInterval(this.upDateTime,1000)
+
+  render() {
+    return(
+        <div>
+          <h2>{this.state.time.toLocaleTimeString()}</h2>
+        </div>
+    )
+  }
+}
+export default App;
+
+
+
